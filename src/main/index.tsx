2890008a07473e997adaf808e7c8d5ad4955aee6
Alexandre Freire Pellegrino <alexandre.pellegrino@simlabsdev.onmicrosoft.com>
2022-04-21 21:57:03 -0300

    chore: finish configuration boilerplate

---
+++ src/main/index.tsx
@@ -0,0 +1,5 @@
+import Teste from "@/presentation/pages/teste";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<Teste />, document.getElementById("main"));
---
+++ src/presentation/pages/teste.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Styles from "./teste.scss";
+
+const Teste: React.FC = () => {
+  return (
+    <div className={Styles.teste}>
+      HELLO
+      <header className={Styles.header}></header>
+      <form className={Styles.form} action=""></form>
+      <footer className={Styles.footer}></footer>
+    </div>
+  );
+};
+
+export default Teste;
